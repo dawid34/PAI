dc86b7f660b461804f327f8c8ee80e7e73a673d7
dawid34 <petrykowskidawid20@gmail.com>
2022-09-13 11:04:32 +0200

    Komentarz

---
+++ lekcja 1 wprowadzenie/zadanie.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+
+  
+<meta charset="utf-8">
+<title></title>
+
+</head>
+<body>
+<?php
+  $mycounter = 1;
+  echo "Hello world";
+    echo "Hello friend";
+	     echo "My counter ". $zmienna;
+
+?>
+<center><h1></h1></center>
+</body>
+</html>
