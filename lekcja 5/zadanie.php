1c1d141c20ab7fd2e400ca2c792840e2f42b2acb
Dawid Petrykowski (Uczeń) <dpetrykowski181@technikumkreatywne.pl>
2022-09-22 10:11:12 +0200

    Komentarz

---
+++ lekcja 5/zadanie.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>zadanie</title>
+</head>
+<body>
+
+	<?php
+		
+		$liczba = 0;
+			for($liczba = -11; $liczba <= 11; ++$liczba)
+			{
+				if($liczba %5 == 0)
+				{	
+					continue;
+				}
+				echo "<liczba>";
+				echo "<br>";
+			
+				
+			}
+		
+		
+	?>
+
+
+</body>
